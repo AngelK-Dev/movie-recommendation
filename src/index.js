3faa0f20e6cda4bfbd895e6a35fa0cf9557e7ab4
ANGEL KALOKIRI <angelkalokiri@gmail.com>
2025-07-18 23:44:03 +0100

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+// src/assets/index.js
+export { default as heroBg } from 'src/assets/hero-bg.png';
+export { default as hero } from 'src/assets/hero.png';
+export { default as logo } from 'src/assets/logo.png';
+export { default as noMovie } from 'src/assets/no-movie.png';
+export { default as reactIcon } from 'src/assets/react.svg';
+export { default as searchIcon } from 'src/assets/search.svg';
+export { default as starIcon } from 'src/assets/star.svg';
